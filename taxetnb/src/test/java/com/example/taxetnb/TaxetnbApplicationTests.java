949d422f32193226359e7983f979ab0284ffe1eb
Anas Naciri <110792944+Naciri-Anas@users.noreply.github.com>
2024-01-14 20:07:56 +0100

    Initial commit

---
+++ taxetnb/src/test/java/com/example/taxetnb/TaxetnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taxetnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxetnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
